fe48f900a6b14f89a6bfec4891a8991e903c098d
dmitry umrikhin <u-dv@yandex.ru>
2024-02-04 19:54:22 +0300

    stage 1 commit

--- diplom_project/asgi.py
+++ diplom_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for orders project.
+ASGI config for diplom_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orders.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diplom_project.settings')
 
 application = get_asgi_application()
